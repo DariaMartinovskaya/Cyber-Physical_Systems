7914b9166124996fc96a4447bafc8340555d5640
DariaMartinovskaya <daria.martinovskaya@gmail.com>
2024-11-09 19:48:00 +0000

    Lesson 2 Update

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,54 @@
 #include <Arduino.h>
 
-int x = 2;
+// //Define LED
+// int LED = 2;
 
-//Check if the LED is working
+// //Check if the LED is working
+// void setup() {
+//   pinMode(LED,OUTPUT);
+// }
+
+// //LED turn on and turn off 
+// void loop() {
+//   digitalWrite(LED,HIGH);
+//   delay(1000);
+
+//   digitalWrite(LED,LOW);
+//   delay(1000);
+
+// }
+
+// Set pin numbers
+// Pushbutton pin
+const int buttonPin = 4; 
+// The number of the LED pin
+const int ledPin = 5;
+
+// VAriable for storing the pushbutton status
+int buttonState = 0;
+
 void setup() {
-  pinMode(x,OUTPUT);
-}
+ Serial.begin(115200);
+ // initialize the pushbutton pin as an input
+ pinMode(buttonPin, INPUT);
+ // initialize the LED pin as an output
+ pinMode(ledPin, OUTPUT);
+ }
 
-//LED turn on and turn off 
-void loop() {
-  digitalWrite(x,HIGH);
-  delay(1000);
-  digitalWrite(x,LOW);
-  delay(1000);
-
-}
+void loop(){
+  // Read the status of the pushbutton value
+  buttonState = digitalRead(buttonPin);
+  Serial.println(buttonState);
+  // Cheack if the pushbutton is pressed.
+  // If it is, the buttonState is HIGH
+  if (buttonState == HIGH){
+    //Turn LED on
+    digitalWrite(ledPin, HIGH);
+  } else {
+    // Turn LED off
+    digitalWrite(ledPin, LOW);
+  }
+  }
 
 // // put function declarations here:
 // int myFunction(int, int);
